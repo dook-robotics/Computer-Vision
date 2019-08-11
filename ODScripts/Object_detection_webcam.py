1692001fec006561441d46624af4e5d6cf29bb9a
Mikian Musser <Bobar312@gmail.com>
2019-08-11 03:07:34 -0700

    Commented OD Script

--- ODScripts/Object_detection_webcam.py
+++ ODScripts/Object_detection_webcam.py
@@ -1,68 +1,45 @@
-######## Webcam Object Detection Using Tensorflow-trained Classifier #########
-#
-# Author: Evan Juras
-# Date: 1/20/18
-# Description:
-# This program uses a TensorFlow-trained classifier to perform object detection.
-# It loads the classifier uses it to perform object detection on a webcam feed.
-# It draws boxes and scores around the objects of interest in each frame from
-# the webcam.
+## Object Detection using TF Model (WebCam) ##
+## Not Pi Compatible                        ##
 
-## Some of the code is copied from Google's example at
-## https://github.com/tensorflow/models/blob/master/research/object_detection/object_detection_tutorial.ipynb
+# Authors:
+#   Mikian Musser
+#   Austin Janushan
 
-## and some is copied from Dat Tran's example at
-## https://github.com/datitran/object_detector_app/blob/master/object_detection_app.py
+# Usage
+#   python Object De.py
 
-## but I changed it to make it more understandable to me.
+# Description
+#   Script to test TF models and tracking algs before ported to Pi
 
-
-# Import packages
+# Imports
 import os
 import cv2
+import sys
 import numpy as np
 import tensorflow as tf
-import sys
-
-# This is needed since the notebook is stored in the object_detection folder.
-sys.path.append("..")
-
-# Import utilites
 from utils import label_map_util
 from utils import visualization_utils as vis_util
 
-threshold = 0.6
-
-# width = 1280
-# height = 720
-
-
-# Name of the directory containing the object detection module we're using
-MODEL_NAME = 'inference_graph'
-
-# Grab path to current working directory
-CWD_PATH = os.getcwd()
-
-# Path to frozen detection graph .pb file, which contains the model that is used
-# for object detection.
-PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,'frozen_inference_graph.pb')
-
-# Path to label map file
-PATH_TO_LABELS = os.path.join(CWD_PATH,'training','labelmap.pbtxt')
+# Constants
+THRESHOLD = 0.6
 
 # Number of classes the object detector can identify
-NUM_CLASSES = 6
+NUM_CLASSES = 1
 
-## Load the label map.
-# Label maps map indices to category names, so that when our convolution
-# network predicts `5`, we know that this corresponds to `king`.
-# Here we use internal utility functions, but anything that returns a
-# dictionary mapping integers to appropriate string labels would be fine
+# File containing the model to use
+MODEL_NAME = 'inference_graph'
+
+# Get file paths
+CWD_PATH = os.getcwd()
+PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,'frozen_inference_graph.pb')
+PATH_TO_LABELS = os.path.join(CWD_PATH,'training','labelmap.pbtxt')
+
+# Load labels.
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
-# Load the Tensorflow model into memory.
+# Load the tf model
 detection_graph = tf.Graph()
 with detection_graph.as_default():
     od_graph_def = tf.GraphDef()
@@ -70,47 +47,40 @@ with detection_graph.as_default():
         serialized_graph = fid.read()
         od_graph_def.ParseFromString(serialized_graph)
         tf.import_graph_def(od_graph_def, name='')
-
     sess = tf.Session(graph=detection_graph)
-
-
-# Define input and output tensors (i.e. data) for the object detection classifier
 
 # Input tensor is the image
 image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
 
-# Output tensors are the detection boxes, scores, and classes
-# Each box represents a part of the image where a particular object was detected
+# Output tensors are the detection boxes, scores, classes, and num objects detected
 detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-
-# Each score represents level of confidence for each of the objects.
-# The score is shown on the result image, together with the class label.
 detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
 detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
-
-# Number of objects detected
 num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 
-# Initialize webcam feed
+# Init webcam
 video = cv2.VideoCapture(0)
-#ret = video.set(3,1280)
-#ret = video.set(4,720)
+
+# Get height/width
+width = video.get(cv2.CAP_PROP_FRAME_WIDTH)   # float
+height = video.get(cv2.CAP_PROP_FRAME_HEIGHT) # float
+
+# Init Tracker and the tracker's bounding box
+tracker = cv2.TrackerMOSSE_create()
+initBB = None
 
 while(True):
 
-    # Acquire frame and expand frame dimensions to have shape: [1, None, None, 3]
-    # i.e. a single-column array, where each item in the column has the pixel RGB value
+    # Get frame from camera
     ret, frame = video.read()
-
     frame_expanded = np.expand_dims(frame, axis=0)
 
-    # Perform the actual detection by running the model with the image as input
+    # Run the frame through the TF model
     (boxes, scores, classes, num) = sess.run(
         [detection_boxes, detection_scores, detection_classes, num_detections],
         feed_dict={image_tensor: frame_expanded})
 
-    # Draw the results of the detection (aka 'visulaize the results')
-    # print("frame")
+    # Draw significant bounding boxes
     vis_util.visualize_boxes_and_labels_on_image_array(
         frame,
         np.squeeze(boxes),
@@ -119,23 +89,52 @@ while(True):
         category_index,
         use_normalized_coordinates=True,
         line_thickness=8,
-        min_score_thresh=threshold)
-    width = video.get(cv2.CAP_PROP_FRAME_WIDTH)   # float
-    height = video.get(cv2.CAP_PROP_FRAME_HEIGHT) # float
-    index = 0;
-    for box in np.squeeze(boxes):
-        ymin = int((box[0]*height))
-        xmin = int((box[1]*width))
-        ymax = int((box[2]*height))
-        xmax = int((box[3]*width))
-        Result = np.array(frame[ymin:ymax,xmin:xmax])
-        if(scores[0][index] >= threshold):
-            cv2.circle(frame,(int((xmin + xmax)/2),int((ymin + ymax)/2)),5,(0,255,0),-1)
-        index = index + 1
-    # print(Result)
-    # All the results have been drawn on the frame, so it's time to display it.
+        min_score_thresh=THRESHOLD)
 
-    # cv2.circle(frame,(int((xmin + xmax)/2),int((ymin + ymax)/2)),5,(0,255,0),-1)
+    # For every box, find the center, draw a dot
+    for index, box in enumerate(np.squeeze(boxes)):
+        if(scores[0][index] >= THRESHOLD):
+            ymin = int((box[0]*height))
+            xmin = int((box[1]*width))
+            ymax = int((box[2]*height))
+            xmax = int((box[3]*width))
+            Result = np.array(frame[ymin:ymax,xmin:xmax])
+            cv2.circle(frame,(int((xmin + xmax)/2),int((ymin + ymax)/2)),5,(0,255,0),-1)
+
+    # Draw the center lines
+    cv2.line(frame, (int(width/2-25),0), (int(width/2-25),int(height)), (0,0,255),5) #left
+    cv2.line(frame, (int(width/2+25),0), (int(width/2+25),int(height)), (0,0,255),5) #right
+
+    # Get the 'primary' card's x value
+    primaryx = int((boxes[0][0][1]*width + boxes[0][0][3]*width)/2)
+
+    # If score is high enough, track object
+    if(scores[0][0] >= 0.9):
+        initBB = (
+            boxes[0][0][1]*width,
+            boxes[0][0][0]*height,
+            boxes[0][0][3]*width-boxes[0][0][1]*width,
+            boxes[0][0][2]*height-boxes[0][0][0]*height)
+        tracker.init(frame, initBB)
+    else:
+        tracker = cv2.TrackerMOSSE_create()
+
+    # Draw trackers bounding object and update tracker
+    if(initBB is not None):
+        (success, box) = tracker.update(frame)
+        if success:
+            (x, y, w, h) = [int(v) for v in box]
+            cv2.rectangle(frame, (x, y), (x + w, y + h),
+            (0, 255, 0), 2)
+
+    # Send instructions
+    if(primaryx > int(width/2+25) and scores[0][0] >= THRESHOLD):
+        print("Move Left")
+    elif(primaryx < int(width/2-25) and scores[0][0] >= THRESHOLD):
+        print("Move Right")
+    elif(primaryx > int(width/2-25) and primaryx < int(width/2+25) and scores[0][0] >= THRESHOLD):
+        print("Move forrward, suck")
+
     cv2.imshow('Object detector', frame)
 
     # Press 'q' to quit
