63877bb54ec88fbc9df578426fc62d55f95e693f
Mikian Musser <Bobar312@gmail.com>
2019-09-11 15:51:35 -0700

    Added 0910 Version

---
+++ ODScripts/0910/motors.py
@@ -0,0 +1,132 @@
+import time
+import RPi.GPIO as GPIO
+
+# Motor Pins
+PWM1=17
+DIR1=22
+PWM2=23
+DIR2=24
+
+# US Pins
+TRIG = 18
+ECHO = 27
+
+USCNT = 0
+
+# programming the GPIO by BCM pin numbers
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+# initialize GPIO Pins for Motor
+GPIO.setup(DIR1,GPIO.OUT)
+GPIO.setup(PWM1,GPIO.OUT)
+GPIO.setup(DIR2,GPIO.OUT)
+GPIO.setup(PWM2,GPIO.OUT)
+
+# initialize GPIO Pins for US
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
+
+# Set frequency of PWM
+pwm = GPIO.PWM(PWM1, 100)
+pwm2 = GPIO.PWM(PWM2, 100)
+
+# Initialize
+pwm.start(0)
+pwm2.start(0)
+
+def stop():
+    print("Stop")
+    pwm.ChangeDutyCycle(0)
+    pwm2.ChangeDutyCycle(0)
+
+def forward():
+    print("Forward")
+    pwm.ChangeDutyCycle(25)
+    pwm2.ChangeDutyCycle(25)
+    GPIO.output(PWM1,GPIO.HIGH)
+    GPIO.output(DIR1,GPIO.LOW)
+    GPIO.output(PWM2,GPIO.HIGH)
+    GPIO.output(DIR2,GPIO.LOW)
+
+def back():
+    print("Back")
+    pwm.ChangeDutyCycle(25)
+    pwm2.ChangeDutyCycle(25)
+    GPIO.output(PWM1,GPIO.LOW)
+    GPIO.output(DIR1,GPIO.HIGH)
+    GPIO.output(PWM2,GPIO.LOW)
+    GPIO.output(DIR2,GPIO.HIGH)
+
+def left():
+    print("Left")
+    pwm.ChangeDutyCycle(25)
+    pwm2.ChangeDutyCycle(0)
+    GPIO.output(PWM1,GPIO.HIGH)
+    GPIO.output(DIR1,GPIO.LOW)
+    GPIO.output(PWM2,GPIO.LOW)
+    GPIO.output(DIR2,GPIO.LOW)
+
+def right():
+    print("Right")
+    pwm2.ChangeDutyCycle(25)
+    pwm.ChangeDutyCycle(0)
+    GPIO.output(PWM1,GPIO.LOW)
+    GPIO.output(DIR1,GPIO.LOW)
+    GPIO.output(PWM2,GPIO.HIGH)
+    GPIO.output(DIR2,GPIO.LOW)
+
+
+def idle():
+    print("Idling")
+
+    global USCNT
+    i = 0 # Needed?
+    avgDistance = 0
+    for i in range(5):
+
+        # Take US Reading
+        GPIO.output(TRIG, False)
+        time.sleep(0.0001)
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
+
+        # Check whether the ECHO is LOW
+        while GPIO.input(ECHO) == 0:
+            pulse_start = time.time()
+
+        # Check whether the ECHO is HIGH
+        while GPIO.input(ECHO) == 1:
+            pulse_end = time.time()
+
+        # Time to get back the pulse to sensor
+        pulse_duration = pulse_end - pulse_start
+
+        #Multiply pulse duration by 17150 (34300/2) to get distance
+        distance = pulse_duration * 17150
+        distance = round(distance, 2)
+        avgDistance = avgDistance + distance
+
+    avgDistance = avgDistance / 5
+    print(avgDistance)
+
+    # Check whether the distance is within 45 cm range
+    # This could be a much better alg, without sleeps...
+    flag = 0
+    if avgDistance < 45:
+        USCNT = USCNT + 1
+        stop()
+        back()
+        if (USCNT == 3) & (flag == 0):
+            USCNT = 0
+            right()
+            flag = 1
+        else:
+            left()
+            flag = 0
+            stop()
+    else:
+        forward()
+        flag = 0
+    return
---
+++ ODScripts/0910/remote.py
@@ -0,0 +1,86 @@
+def ps4():
+    # Get the currently pressed keys on the keyboard
+    events = pygame.event.get()
+    for event in events:
+        # Keys have changed, generate the command list based on keys
+        if event.type == pygame.JOYBUTTONDOWN:
+            if j.get_button(0):
+                print("Pressed: X")
+                return 2
+            elif j.get_button(1):
+                print("Pressed: Circle")
+                return 4
+            elif j.get_button(2):
+                print("Pressed: Triangle")
+                return 3
+            elif j.get_button(3):
+                print("Pressed: Square")
+                return 5
+            elif j.get_button(4):
+                print("Pressed: L1")
+            elif j.get_button(5):
+                print("Pressed: R1")
+            elif j.get_button(6):
+                print("Pressed: L2")
+            elif j.get_button(7):
+                print("Pressed: R2")
+            elif j.get_button(8):
+                print("Pressed: SHARE")
+            elif j.get_button(9):
+                print("Pressed: OPTIONS")
+                return 1
+            elif j.get_button(10):
+                print("Pressed: PS Button")
+                return 6
+            elif j.get_button(11):
+                print("Pressed: Left Analog")
+            elif j.get_button(12):
+                print("Pressed: Right Analog")
+    return 0
+
+def ps4Stick():
+    # Get the currently pressed keys on the keyboard
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.JOYAXISMOTION:
+            moveUp = False
+            moveDown = False
+            moveRight = False
+            moveLeft = False
+
+            # A joystick has been moved, read axis positions (-1 to +1)
+            upDown = j.get_axis(axisUpDown)
+            leftRight = j.get_axis(axisLeftRight)
+
+            # Invert any axes which are incorrect
+            if axisUpDownInverted:
+                upDown = -upDown
+            if axisLeftRightInverted:
+                leftRight = -leftRight
+
+            # Determine Up / Down values
+            if upDown < -0.1:
+                moveUp = True
+                moveDown = False
+            elif upDown > 0.1:
+                moveUp = False
+                moveDown = True
+            else:
+                moveUp = False
+                moveDown = False
+
+            # Determine Left / Right values
+            if leftRight < -0.1:
+                moveLeft = True
+                moveRight = False
+            elif leftRight > 0.1:
+                moveLeft = False
+                moveRight = True
+            else:
+                moveLeft = False
+                moveRight = False
+            return (upDown, leftRight)
+    return (0,0)
+
+def checkConnect():
+    return pygame.joystick.get_count()
